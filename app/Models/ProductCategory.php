218dbaa9b317079b05f578c0c0a0692a4311058a
Parakram <parakram@keemail.me>
2025-07-06 09:59:46 +0545

    Model and Migration for ProductCategory

--- app/Models/ProductCategory.php
+++ app/Models/ProductCategory.php
@@ -2,9 +2,42 @@
 
 namespace App\Models;
 
+use Eloquent;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Carbon;
 
+/**
+ * 
+ *
+ * @property int $id
+ * @property string $name
+ * @property string|null $description
+ * @property int $status
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * @method static Builder<static>|ProductCategory newModelQuery()
+ * @method static Builder<static>|ProductCategory newQuery()
+ * @method static Builder<static>|ProductCategory query()
+ * @method static Builder<static>|ProductCategory whereCreatedAt($value)
+ * @method static Builder<static>|ProductCategory whereDescription($value)
+ * @method static Builder<static>|ProductCategory whereId($value)
+ * @method static Builder<static>|ProductCategory whereName($value)
+ * @method static Builder<static>|ProductCategory whereStatus($value)
+ * @method static Builder<static>|ProductCategory whereUpdatedAt($value)
+ * @mixin Eloquent
+ */
 class ProductCategory extends Model
 {
-    //
+    use HasFactory;
+
+    protected $table = 'product_categories';
+    protected $fillable = [
+        'name',
+        'description',
+        'status',
+    ];
+    protected $primaryKey = 'id';
+
 }
