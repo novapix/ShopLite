a873a817ba63b611da0a457e16c0b1b9161aa514
Parakram <parakram@keemail.me>
2025-07-14 11:11:15 +0545

    Refactor role checking and update docblocks in auth controllers

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -26,7 +26,6 @@ class RegisteredUserController extends Controller
     /**
      * Handle an incoming registration request.
      *
-     * @throws \Illuminate\Validation\ValidationException
      */
     public function store(Request $request): RedirectResponse
     {
--- app/Http/Middleware/CheckRole.php
+++ app/Http/Middleware/CheckRole.php
@@ -7,26 +7,24 @@ use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Illuminate\Support\Facades\Auth;
 
-namespace App\Http\Middleware;
-
-
-
 class CheckRole
 {
     /**
      * Handle an incoming request.
+     *
      * @param  Request  $request
      * @param  Closure  $next
      * @param  mixed  ...$roles  // Accept multiple roles
      * @return Response
      */
-    public function handle(Request $request, Closure $next, ...$roles): Response {
+    public function handle(Request $request, Closure $next, ...$roles): Response
+    {
         if (!Auth::check()) {
             return redirect()->route('login');
         }
 
-        $user = Auth::user()->loadMissing('role');
-        $userRole = $user->role->role ?? null;
+        $user = Auth::user()->loadMissing('roles');
+        $userRole = $user->roles->role ?? null;
 
         if (in_array($userRole, $roles)) {
             return $next($request);
