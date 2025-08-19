26d933aa09ca161cd04c638d959fe3f1b70854b6
Parakram <parakram@keemail.me>
2025-08-19 13:21:08 +0545

    API Handling
    force json res for api endpoint

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -9,8 +9,10 @@ use Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        apiPrefix: 'api/admin',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
@@ -21,11 +23,36 @@ return Application::configure(basePath: dirname(__DIR__))
             HandleInertiaRequests::class,
             AddLinkHeadersForPreloadedAssets::class,
         ]);
+
+        // Force JSON for all API requests
+       $middleware->api(append: [
+            \App\Http\Middleware\ForceJsonApi::class,
+        ]);
         $middleware->alias([
             'role' => \App\Http\Middleware\CheckRole::class,
             'role.redirect' => \App\Http\Middleware\RoleBasedRedirect::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
-    })->create();
+        // Handle unauthenticated API requests
+        $exceptions->renderable(function (\Illuminate\Auth\AuthenticationException $e, $request) {
+            if ($request->is('api/*')) {
+                return response()->json([
+                    'status'  => 'error',
+                    'message' => 'Unauthenticated. You must provide a valid API token.',
+                    'guards'  => $e->guards(), // shows which guard(s) failed
+                ], 401);
+            }
+        });
+
+
+        // Handle other exceptions for API routes
+        $exceptions->renderable(function (\Throwable $e, $request) {
+            if ($request->is('api/*')) {
+                return response()->json([
+                    'message' => $e->getMessage(),
+                ], method_exists($e, 'getStatusCode') ? $e->getStatusCode() : 500);
+            }
+        });
+    })
+    ->create();
